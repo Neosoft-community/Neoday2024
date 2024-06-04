0ae8d7480706b8c4e5ed5828556520e88b1b5283
Fabien GUILLOU <f.guillou@NeoSoft.local>
2024-06-04 14:51:52 +0200

    mise en place de boutons pour la creation et envoie de message plutot que les zones dans les place de parking (meme si c'etait plus amusant)

--- src/main.ts
+++ src/main.ts
@@ -170,7 +170,7 @@ WA.onInit().then(() => {
     })
 
     WA.room.area.onLeave('indice9').subscribe(closePopup=*/
-    
+
 
     WA.room.area.onEnter('rs').subscribe(() => {
         currentPopup = WA.ui.openPopup("rsPopup", "Tu veux nous suivre sur les réseaux sociaux ?", [{
@@ -214,117 +214,330 @@ WA.onInit().then(() => {
     }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
+
+let allPopup: Array<any> = new Array()
+
+WA.room.onEnterLayer("mgZoneAdmin").subscribe(() => {
+
+    
+    console.log("testage du fonctionnage")
+    const playerName = WA.player.name;
+
+    if(playerName != nomAdmin){
+        return;
+    }
+    WA.controls.disablePlayerControls()
+    WA.ui.openPopup("mgSalle1", 'salle 1', [{
+        label: "ajouter",
+        className: "primary",
+        callback: (popup) => {
+            console.log(popup)
+            // Close the popup when the "Close" button is pressed.
+            addTextMg("salle 1,")
+            //popup.close();
+            
+        }
+    }]);
+    WA.ui.openPopup("mgSalle2", 'salle 2', [{
+        label: "ajouter",
+        className: "primary",
+        callback: (popup) => {
+            console.log(popup)
+            // Close the popup when the "Close" button is pressed.
+            addTextMg("salle 2,")
+            //popup.close();
+            
+        }
+    }]);
+    WA.ui.openPopup("mgSalle3", 'salle 3', [{
+        label: "ajouter",
+        className: "primary",
+        callback: (popup) => {
+            console.log(popup)
+            // Close the popup when the "Close" button is pressed.
+            addTextMg("salle 3,")
+            //popup.close();
+            
+        }
+    }]);
+    WA.ui.openPopup("mgSalle4", 'salle 4', [{
+        label: "ajouter",
+        className: "primary",
+        callback: (popup) => {
+            console.log(popup)
+            // Close the popup when the "Close" button is pressed.
+            addTextMg("salle 4,")
+            //popup.close();
+            
+        }
+    }]);
+    WA.ui.openPopup("mgSalle5", 'salle 5', [{
+        label: "ajouter",
+        className: "primary",
+        callback: (popup) => {
+            console.log(popup)
+            // Close the popup when the "Close" button is pressed.
+            addTextMg("salle 5,")
+            //popup.close();
+            
+        }
+    }]);
+    WA.ui.openPopup("mgSalle6", 'salle 6', [{
+        label: "ajouter",
+        className: "primary",
+        callback: (popup) => {
+            console.log(popup)
+            // Close the popup when the "Close" button is pressed.
+            addTextMg("salle 6,")
+            //popup.close();
+            
+        }
+    }]);
+    WA.ui.openPopup("mgSalle7", 'salle 7', [{
+        label: "ajouter",
+        className: "primary",
+        callback: (popup) => {
+            console.log(popup)
+            // Close the popup when the "Close" button is pressed.
+            addTextMg("salle 7,")
+            //popup.close();
+            
+        }
+    }]);
+    WA.ui.openPopup("mgSalle8", 'salle 8', [{
+        label: "ajouter",
+        className: "primary",
+        callback: (popup) => {
+            console.log(popup)
+            // Close the popup when the "Close" button is pressed.
+            addTextMg("salle 8,")
+            //popup.close();
+            
+        }
+    }]);
+    WA.ui.openPopup("mgSalleAmphi", 'Amphi', [{
+        label: "ajouter",
+        className: "primary",
+        callback: (popup) => {
+            console.log(popup)
+            // Close the popup when the "Close" button is pressed.
+            addTextMg("amphithéâtre,")
+            //popup.close();
+            
+        }
+    }]);
+    WA.ui.openPopup("5min", '5 minutes de retard', [{
+        label: "ajouter",
+        className: "primary",
+        callback: (popup) => {
+            console.log(popup)
+            // Close the popup when the "Close" button is pressed.
+            addTextMg("5 minutes de retard.  ")
+            //popup.close();
+            
+        }
+    }]);
+    WA.ui.openPopup("10min", '10 minutes de retard', [{
+        label: "ajouter",
+        className: "primary",
+        callback: (popup) => {
+            console.log(popup)
+            // Close the popup when the "Close" button is pressed.
+            addTextMg("10 minutes de retard.  ")
+            //popup.close();
+            
+        }
+    }]);
+    WA.ui.openPopup("15min", '15 minutes de retard', [{
+        label: "ajouter",
+        className: "primary",
+        callback: (popup) => {
+            console.log(popup)
+            // Close the popup when the "Close" button is pressed.
+            addTextMg("15 minutes de retard.  ")
+            //popup.close();
+            
+        }
+    }]);
+    WA.ui.openPopup("20min", '20 minutes de retard', [{
+        label: "ajouter",
+        className: "primary",
+        callback: (popup) => {
+            console.log(popup)
+            // Close the popup when the "Close" button is pressed.
+            addTextMg("20 minutes de retard.  ")
+            //popup.close();
+            
+        }
+    }]);
+    WA.ui.openPopup("vasCommencer", 'debut de conference', [{
+        label: "ajouter",
+        className: "primary",
+        callback: (popup) => {
+            console.log(popup)
+            // Close the popup when the "Close" button is pressed.
+            addTextMg("la conférence commenceras sous peu.  ")
+            //popup.close();
+            
+        }
+    }]);
+    WA.ui.openPopup("admin", 'Envoyer', [{
+        label: "envoyé",
+        className: "primary",
+        callback: (popup) => {
+            console.log(popup)
+            // Close the popup when the "Close" button is pressed.
+            sendMg();
+            //popup.close();
+            
+        }
+    }]);
+    WA.ui.openPopup("mgSuppression", 'Supprimer', [{
+        label: "supprimer",
+        className: "primary",
+        callback: (popup) => {
+            console.log(popup)
+            // Close the popup when the "Close" button is pressed.
+            resetMg();
+            //popup.close();
+            
+        }
+    }]);
+    WA.ui.openPopup("mgTest", 'preview', [{
+        label: "preview",
+        className: "primary",
+        callback: (popup) => {
+            console.log(popup)
+            // Close the popup when the "Close" button is pressed.
+            globalMessage(WA.state.messGlob);
+            //popup.close();
+            
+        }
+    }]);
+})
+// Close the popup when we leave the zone.
+WA.room.onLeaveLayer("mgZoneAdmin").subscribe(() => {
+    for(let pop in allPopup){
+        pop
+    }
+    //helloWorldPopup.close();
+})
+
+
+
+
 function closePopup(){
     if (currentPopup !== undefined) {
         currentPopup.close();
         currentPopup = undefined;
     }
 }
-WA.room.area.onEnter('mgSalle1').subscribe(() =>{
-    const playerName = WA.player.name;
-    if(playerName === nomAdmin){
-        WA.state.messGlob = WA.state.messGlob + "salle 1, ";
-    }
-})
-WA.room.area.onEnter('mgSalle2').subscribe(() =>{
-    const playerName = WA.player.name;
-    if(playerName === nomAdmin){
-        WA.state.messGlob = WA.state.messGlob + "salle 2, ";
-    }
-})
-WA.room.area.onEnter('mgSalle3').subscribe(() =>{
-    const playerName = WA.player.name;
-    if(playerName === nomAdmin){
-        WA.state.messGlob = WA.state.messGlob + "salle 3, ";
-    }
-})
-WA.room.area.onEnter('mgSalle4').subscribe(() =>{
-    const playerName = WA.player.name;
-    if(playerName === nomAdmin){
-        WA.state.messGlob = WA.state.messGlob + "salle 4, ";
-    }
-})
-WA.room.area.onEnter('mgSalle5').subscribe(() =>{
-    const playerName = WA.player.name;
-    if(playerName === nomAdmin){
-        WA.state.messGlob = WA.state.messGlob + "salle 5, ";
-    }
-})
-WA.room.area.onEnter('mgSalle6').subscribe(() =>{
-    const playerName = WA.player.name;
-    if(playerName === nomAdmin){
-        WA.state.messGlob = WA.state.messGlob + "salle 6, ";
-    }
-})
-WA.room.area.onEnter('mgSalle7').subscribe(() =>{
-    const playerName = WA.player.name;
-    if(playerName === nomAdmin){
-        WA.state.messGlob = WA.state.messGlob + "salle 7, ";
-    }
-})
-WA.room.area.onEnter('mgSalle8').subscribe(() =>{
-    const playerName = WA.player.name;
-    if(playerName === nomAdmin){
-        WA.state.messGlob = WA.state.messGlob + "salle 8, ";
-    }
-})
-WA.room.area.onEnter('mgSalleAmphi').subscribe(() =>{
-    const playerName = WA.player.name;
-    if(playerName === nomAdmin){
-        WA.state.messGlob = WA.state.messGlob + "Ampphitéâtre, ";
-    }
-})
-WA.room.area.onEnter('vasCommencer').subscribe(() =>{
-    const playerName = WA.player.name;
-    if(playerName === nomAdmin){
-        WA.state.messGlob = WA.state.messGlob + "l'intervention commenceras sous peu.| ";
-    }
-})
-WA.room.area.onEnter('5min').subscribe(() =>{
-    const playerName = WA.player.name;
-    if(playerName === nomAdmin){
-        WA.state.messGlob = WA.state.messGlob + "retard d'environ 5 minutes.| ";
-    }
-})
-WA.room.area.onEnter('10min').subscribe(() =>{
-    const playerName = WA.player.name;
-    if(playerName === nomAdmin){
-        WA.state.messGlob = WA.state.messGlob + "retard d'environ 10 minutes.| ";
-    }
-})
-WA.room.area.onEnter('15min').subscribe(() =>{
-    const playerName = WA.player.name;
-    if(playerName === nomAdmin){
-        WA.state.messGlob = WA.state.messGlob + "retard d'environ 15 minutes.| ";
-    }
-})
-WA.room.area.onEnter('20min').subscribe(() =>{
-    const playerName = WA.player.name;
-    if(playerName === nomAdmin){
-        WA.state.messGlob = WA.state.messGlob + "retard d'environ 20 minutes.| ";
-    }
-})
-WA.room.area.onEnter('mgSuppression').subscribe(() =>{
-    const playerName = WA.player.name;
-    if(playerName === nomAdmin){
-        WA.state.messGlob = "Attention: ";
-    }
-})
-WA.room.area.onEnter('mgTest').subscribe(() =>{
-    const playerName = WA.player.name;
-    if(playerName === nomAdmin){
-        globalMessage(WA.state.messGlob);
-    }
-})
-WA.room.area.onEnter('admin').subscribe(() =>{
+// WA.room.area.onEnter('mgSalle1').subscribe(() =>{
+//     const playerName = WA.player.name;
+//     if(playerName === nomAdmin){
+//         WA.state.messGlob = WA.state.messGlob + "salle 1, ";
+//     }
+// })
+// WA.room.area.onEnter('mgSalle2').subscribe(() =>{
+//     const playerName = WA.player.name;
+//     if(playerName === nomAdmin){
+//         WA.state.messGlob = WA.state.messGlob + "salle 2, ";
+//     }
+// })
+// WA.room.area.onEnter('mgSalle3').subscribe(() =>{
+//     const playerName = WA.player.name;
+//     if(playerName === nomAdmin){
+//         WA.state.messGlob = WA.state.messGlob + "salle 3, ";
+//     }
+// })
+// WA.room.area.onEnter('mgSalle4').subscribe(() =>{
+//     const playerName = WA.player.name;
+//     if(playerName === nomAdmin){
+//         WA.state.messGlob = WA.state.messGlob + "salle 4, ";
+//     }
+// })
+// WA.room.area.onEnter('mgSalle5').subscribe(() =>{
+//     const playerName = WA.player.name;
+//     if(playerName === nomAdmin){
+//         WA.state.messGlob = WA.state.messGlob + "salle 5, ";
+//     }
+// })
+// WA.room.area.onEnter('mgSalle6').subscribe(() =>{
+//     const playerName = WA.player.name;
     
-    const playerName = WA.player.name;
-    if(playerName === nomAdmin){
-        WA.state.aAfficher = true;
-        //globalMessage( WA.state.messGlob ); 
-    } 
+//     if(playerName === nomAdmin){
+//         WA.state.messGlob = WA.state.messGlob + "salle 6, ";
+//     }
+// })
+// WA.room.area.onEnter('mgSalle7').subscribe(() =>{
+//     const playerName = WA.player.name;
+//     if(playerName === nomAdmin){
+//         WA.state.messGlob = WA.state.messGlob + "salle 7, ";
+//     }
+// })
+// WA.room.area.onEnter('mgSalle8').subscribe(() =>{
+//     const playerName = WA.player.name;
+//     if(playerName === nomAdmin){
+//         WA.state.messGlob = WA.state.messGlob + "salle 8, ";
+//     }
+// })
+// WA.room.area.onEnter('mgSalleAmphi').subscribe(() =>{
+//     const playerName = WA.player.name;
+//     if(playerName === nomAdmin){
+//         WA.state.messGlob = WA.state.messGlob + "Ampphitéâtre, ";
+//     }
+// })
+// WA.room.area.onEnter('vasCommencer').subscribe(() =>{
+//     const playerName = WA.player.name;
+//     if(playerName === nomAdmin){
+//         WA.state.messGlob = WA.state.messGlob + "l'intervention commenceras sous peu.| ";
+//     }
+// })
+// WA.room.area.onEnter('5min').subscribe(() =>{
+//     const playerName = WA.player.name;
+//     if(playerName === nomAdmin){
+//         WA.state.messGlob = WA.state.messGlob + "retard d'environ 5 minutes.| ";
+//     }
+// })
+// WA.room.area.onEnter('10min').subscribe(() =>{
+//     const playerName = WA.player.name;
+//     if(playerName === nomAdmin){
+//         WA.state.messGlob = WA.state.messGlob + "retard d'environ 10 minutes.| ";
+//     }
+// })
+// WA.room.area.onEnter('15min').subscribe(() =>{
+//     const playerName = WA.player.name;
+//     if(playerName === nomAdmin){
+//         WA.state.messGlob = WA.state.messGlob + "retard d'environ 15 minutes.| ";
+//     }
+// })
+// WA.room.area.onEnter('20min').subscribe(() =>{
+//     const playerName = WA.player.name;
+//     if(playerName === nomAdmin){
+//         WA.state.messGlob = WA.state.messGlob + "retard d'environ 20 minutes.| ";
+//     }
+// })
+// WA.room.area.onEnter('mgSuppression').subscribe(() =>{
+//     const playerName = WA.player.name;
+//     if(playerName === nomAdmin){
+//         WA.state.messGlob = "Attention: ";
+//     }
+// })
+// WA.room.area.onEnter('mgTest').subscribe(() =>{
+//     const playerName = WA.player.name;
+//     if(playerName === nomAdmin){
+//         globalMessage(WA.state.messGlob);
+//     }
+// })
+// WA.room.area.onEnter('admin').subscribe(() =>{
     
-})
+//     const playerName = WA.player.name;
+//     if(playerName === nomAdmin){
+//         WA.state.aAfficher = true;
+//         //globalMessage( WA.state.messGlob ); 
+//     } 
+    
+// })
 function globalMessage(message:any){
     WA.ui.banner.openBanner({
         id: "banner",
@@ -343,10 +556,20 @@ function affichager():void{
             count = count + 1;
         }else if(WA.player.name == nomAdmin && count == 1){
             count              = 0;
-            WA.state.messGlob  = "Attention: ";
+            WA.state.messGlob  = "votre attention: ";
             WA.state.aAfficher = false;
         }
     }
     setTimeout(affichager,2000);
 }
+function addTextMg(txt:string):void{
+    WA.state.messGlob   = WA.state.messGlob + " " + txt;  
+}
+function sendMg():void{
+    WA.state.aAfficher = true;
+}
+function resetMg():void{
+    WA.state.messGlob = "Votre attention:"
+}
+
 export {};

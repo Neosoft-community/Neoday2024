8aa9a61988afa2c17b4341fc0b13fc57e7d14acd
Tom PAIN <t.pain@NeoSoft.local>
2024-06-11 15:13:00 +0200

    final map and main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,13 @@
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
-let currentPopup: any = undefined;
 let allPopup: Array<any> = new Array()
 
-const nomAdmin = "Louis Evrard"
+const nomAdmin = "Tom Pain"
 let count:number =0;
 // Waiting for the API to be ready
 WA.onInit().then(() => {
-    
+    WA.player.state.foo = 0
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
     affichager();
@@ -54,119 +53,119 @@ WA.onInit().then(() => {
     // Vérifier l'heure toutes les secondes
     setInterval(verifierHeure, 1000);
 
-    /*WA.room.area.onEnter('indice1').subscribe(() => {
+    WA.room.area.onEnter('indice1').subscribe(() => {
         // const today = new Date();
         // const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("indice1Popup", "Bonjour ! Voici mon indice : Je suis le Practice leader de la Practice DEVOPS, la lettre que tu recherches est la première de mon prénom !", [{
+        allPopup.push(WA.ui.openPopup("indice1Popup", "Bonjour ! Voici mon indice : C'est 5 en romain !", [{
             label: "OK !",
             className: "primary",
-            callback: (popup) => {
-                popup.close();
+            callback: () => {
+                closePopup();
             }
-        }]);
-    })*/
+        }]));
+    })
 
-   /* WA.room.area.onLeave('indice1').subscribe(closePopup)
+    WA.room.area.onLeave('indice1').subscribe(closePopup)
 
     WA.room.area.onEnter('indice2').subscribe(() => {
-        currentPopup = WA.ui.openPopup("indice2Popup", "Voici un nouvel indice : J'ai ouvert le forum des communauté et je suis le PDG, la lettre que tu recherches est la première de mon prenom !", [{
+        allPopup.push (WA.ui.openPopup("indice2Popup", "Voici un nouvel indice : C'est la meilleure note possible à l'école !", [{
             label: "Merci !",
             className: "primary",
-            callback: (popup) => {
-                popup.close();
+            callback: () => {
+                closePopup();
             }
-        }]);
-    })*/
+        }]));
+    })
 
-  /*  WA.room.area.onLeave('indice2').subscribe(closePopup)
+    WA.room.area.onLeave('indice2').subscribe(closePopup)
 
     WA.room.area.onEnter('indice3').subscribe(() => {
-        currentPopup = WA.ui.openPopup("indice3Popup", "Un nouvel indice, ici tu trouveras : Je suis le Practice leader de la Practice INFRA / CLOUD, la lettre que tu recherches est la quatrième de mon prénom !", [{
+        allPopup.push (WA.ui.openPopup("indice3Popup", "Un nouvel indice, ici tu trouveras : Mon 3eme est à la même place dans l'alphabet !", [{
             label: "Merci !",
             className: "primary",
-            callback: (popup) => {
-                popup.close();
+            callback: () => {
+                closePopup();
             }
-        }]);
-    })*/
+        }]));
+    })
 
-  /*  WA.room.area.onLeave('indice3').subscribe(closePopup)*/
+    WA.room.area.onLeave('indice3').subscribe(closePopup)
 
-  /*  WA.room.area.onEnter('indice4').subscribe(() => {
-        currentPopup = WA.ui.openPopup("indice4Popup", "Bonjour, voici mon indice : Je suis un des Practice leader de la Practice CYBER mais pas Sylvain, la lettre que tu recherches est la septième de mon prénom !", [{
+    WA.room.area.onEnter('indice4').subscribe(() => {
+        allPopup.push (WA.ui.openPopup("indice4Popup", "Bonjour, voici mon indice : Je suis l'onomatopée préférée de Denis Brognard !", [{
             label: "OK, merci !",
             className: "primary",
-            callback: (popup) => {
-                popup.close();
+            callback: () => {
+                closePopup();
             }
-        }]);
-    })*/
+        }]));
+    })
 
-   /* WA.room.area.onLeave('indice4').subscribe(closePopup)*/
+    WA.room.area.onLeave('indice4').subscribe(closePopup)
 
-  /*  WA.room.area.onEnter('indice5').subscribe(() => {
-        currentPopup = WA.ui.openPopup("indice5Popup", "Salut ! Allez, voici un petit indice : Je suis le Practice leader de la Practice AGILITE, la lettre que tu recherches est la quatrième de mon prénom !", [{
+    WA.room.area.onEnter('indice5').subscribe(() => {
+        allPopup.push (WA.ui.openPopup("indice5Popup", "Salut ! Allez, voici un petit indice : Je suis la première lettre du nom de notre entreprise !", [{
             label: "Merci !",
             className: "primary",
-            callback: (popup) => {
-                popup.close();
+            callback: () => {
+                closePopup();
             }
-        }]);
+        }]));
     })
 
-    WA.room.area.onLeave('indice5').subscribe(closePopup)*/
+    WA.room.area.onLeave('indice5').subscribe(closePopup)
 
-   /* WA.room.area.onEnter('indice6').subscribe(() => {
-        currentPopup = WA.ui.openPopup("indice6Popup", "Bonjour ! Voici mon indice : Je suis le Social Media Manager de Néosoft, la lettre de que tu recherches est la dernière de mon prénom !", [{
+    WA.room.area.onEnter('indice6').subscribe(() => {
+        allPopup.push (WA.ui.openPopup("indice6Popup", "Bonjour ! Voici mon indice : Je suis l'initiale de la troisième valeur chez Néosoft !", [{
             label: "OK, merci !",
             className: "primary",
-            callback: (popup) => {
-                popup.close();
+            callback: () => {
+                closePopup();
             }
-        }]);
+        }]));
     })
 
-    WA.room.area.onLeave('indice6').subscribe(closePopup)*/
+    WA.room.area.onLeave('indice6').subscribe(closePopup)
 
-   /* WA.room.area.onEnter('indice7').subscribe(() => {
-        currentPopup = WA.ui.openPopup("indice7Popup", "Hello ! Indice : Je suis la Responsable formation Néosoft Training, la lettre que tu recherches est la première de mon prénom !", [{
+    WA.room.area.onEnter('indice7').subscribe(() => {
+        allPopup.push (WA.ui.openPopup("indice7Popup", "Hello ! Indice : Je suis imprimé sur la poitrine d'un super héros très connu !", [{
             label: "OK, merci !",
             className: "primary",
-            callback: (popup) => {
-                popup.close();
+            callback: () => {
+                closePopup();
             }
-        }]);
+        }]));
     })
 
-    WA.room.area.onLeave('indice7').subscribe(closePopup)*/
+    WA.room.area.onLeave('indice7').subscribe(closePopup)
 
-   /* WA.room.area.onEnter('indice8').subscribe(() => {
-        currentPopup = WA.ui.openPopup("indice8Popup", "Indice : Je suis le Practice leader de la Practice Software Engineering, la lettre que tu recherches est la deuxième de mon prénom !", [{
+    WA.room.area.onEnter('indice8').subscribe(() => {
+        allPopup.push ( WA.ui.openPopup("indice8Popup", "Indice : Je suis la lettre la plus utilisée dans la langue française !", [{
             label: "OK, merci !",
             className: "primary",
-            callback: (popup) => {
-                popup.close();
+            callback: () => {
+                closePopup();
             }
-        }]);
+        }]));
     })
 
-    WA.room.area.onLeave('indice8').subscribe(closePopup)*/
+    WA.room.area.onLeave('indice8').subscribe(closePopup)
 
-   /* WA.room.area.onEnter('indice9').subscribe(() => {
-        currentPopup = WA.ui.openPopup("indice9Popup", "Mon tout est l'anagramme d'une plante connue pour ses jolies fleurs bleues !", [{
+    WA.room.area.onEnter('indice9').subscribe(() => {
+        allPopup.push (WA.ui.openPopup("indice9Popup", "Mon tout est le moment que tout le monde attend !", [{
             label: "Rendez-vous dans l'enquete de satisfaction pour répondre !",
             className: "primary",
-            callback: (popup) => {
-                popup.close();
+            callback: () => {
+                closePopup();
             }
-        }]);
+        }]));
     })
 
-    WA.room.area.onLeave('indice9').subscribe(closePopup)*/
+    WA.room.area.onLeave('indice9').subscribe(closePopup)
 
 
     WA.room.area.onEnter('rs').subscribe(() => {
-        currentPopup = WA.ui.openPopup("rsPopup", "Tu veux nous suivre sur les réseaux sociaux ?", [{
+        allPopup.push(WA.ui.openPopup("rsPopup", "Tu veux nous suivre sur les réseaux sociaux ?", [{
             label: "Linkedin",
             className: "primary",
             callback: (popup) => {
@@ -182,31 +181,197 @@ WA.onInit().then(() => {
                 WA.openTab('https://twitter.com/neosoft_'),
                 popup.close();
             }
-        }]);
+        }]));
 
     })
 
     WA.room.area.onLeave('rs').subscribe(closePopup)
 
     WA.room.area.onEnter('fdn').subscribe(() => {
-        currentPopup = WA.ui.openPopup("fdnPopup", "Viens te sensibiliser aux enjeux du numérique !", [{
+        allPopup.push(WA.ui.openPopup("fdnPopup", "Viens te sensibiliser aux enjeux du numérique !", [{
             label: "Je m'inscris à une fresque",
             className: 'primary',
             callback: (popup) => {
                 WA.openTab('https://landing.neosoft.fr/numerique-responsable'),
                 popup.close();
             }
-        }]);
+        }]));
     })
 
     WA.room.area.onLeave('fdn').subscribe(closePopup)
 
+    WA.room.area.onEnter('bot1').subscribe(() => {
+        let test: any;
+        test = WA.player.state.foo,
+        WA.player.state.foo = test;
+        if (test == 0)
+        {
+            allPopup.push( WA.ui.openPopup("bot1Popup", "Je sais qu'il y a un trésor dans le coin mais j'ai trop peur de l'eau.. Mon frère jumeau est parti bronzer au nord, tu pourrais y aller pour moi ?", [{
+                label: "Bien sûr !",
+                className: 'primary',
+                callback: () => {
+
+                    closePopup();               
+                }
+           
+            }]));
+        WA.player.state.foo = test+1
+        console.log("test if : ", WA.player.state.foo)
+        }
+        else
+        {
+            allPopup.push( WA.ui.openPopup("bot1Popup", "Je t'ai déjà dit d'aller à la piscine au nord !", [{
+                label: "D'accord..",
+                className: 'primary',
+                callback: () => {
+                closePopup();
+                }
+            }]));
+            console.log("test else : ", WA.player.state.foo)
+        }
+        })
+    
+
+    WA.room.area.onLeave('bot1').subscribe(closePopup)
+
+
+
+    WA.room.area.onEnter('bot2').subscribe(() => {
+        let test: any;
+        test = WA.player.state.foo,
+        WA.player.state.foo = test;
+        
+
+        if (test < 1)
+            {
+                allPopup.push(WA.ui.openPopup("bot2Popup", "Laisse moi bronzer, tu veux bien ?", [{
+                    label: "Euuuh.. ok ?",
+                    className: 'primary',
+                    callback: () => {
+                        closePopup();
+                    }
+                }]));
+    
+            }
+
+        else if (test == 1)
+        {
+            allPopup.push(WA.ui.openPopup("bot2Popup", "Un trésor ? Je n'en sais rien, essaye d'aller parler à mon cousin.. Je crois qu'il n'ose pas rentrer dans le labyrinthe, tu le trouveras là bas !", [{
+                    label: "Bien sûr !",
+                    className: 'primary',
+                    callback: () => {
+                        closePopup();
+                    }
+               
+                }]));
+            WA.player.state.foo = test+1
+    
+        }
+
+
+        else
+        {
+            allPopup.push(WA.ui.openPopup("bot2Popup", "Es-tu allé au labyrinthe ?", [{
+                    label: "Pas encore",
+                    className: 'primary',
+                    callback: () => {
+                        closePopup();
+                    }
+                }]));
+    
+        }
+        })
+    
+
+    WA.room.area.onLeave('bot2').subscribe(closePopup)
+
+    WA.room.area.onEnter('bot3').subscribe(() => {
+        let test: any;
+        test = WA.player.state.foo,
+        WA.player.state.foo = test;
+        
+        if (test == 2)
+            {
+                allPopup.push(WA.ui.openPopup("bot3Popup", "Au Nord-Est tu iras, cette pelle tu prendras, et à la croix tu te rendras..", [{
+                    label: "Alors il existe ?",
+                    className: 'primary',
+                    callback: () => {
+                        closePopup();
+                    }
+               
+                }]));
+            WA.player.state.foo = test+1
+    
+            }
+            
+
+        if (test < 2)
+        {
+            allPopup.push(WA.ui.openPopup("bot3Popup", "Reviens plus tard...", [{
+                label: "Ok",
+                className: 'primary',
+                callback: () => {
+                    closePopup();
+                }
+            }]));
+
+        }
+
+            
+        if (test > 2)
+            {
+                allPopup.push(WA.ui.openPopup("bot3Popup", "Bon, tu l'as récupéré ce trésor ?", [{
+                    label: "Non, pas encore",
+                    className: 'primary',
+                    callback: () => {
+                    closePopup();
+                    }
+                }]));
+    
+            }
+        })
+    
+
+    WA.room.area.onLeave('bot3').subscribe(closePopup)
+
+    WA.room.area.onEnter('tresor').subscribe(() => {
+        let test: any;
+        test = WA.player.state.foo,
+        WA.player.state.foo = test;
+        if (test <= 2)
+        {
+            allPopup.push(WA.ui.openPopup("tresorPopup1", "Je me demande ce qu'il se cache ici.. Tiens un ticket de parking ? Peut être que quelqu'un l'a égaré ? Je ferai bien d'aller voir là bas..", [{
+                label: "Fermer",
+                className: 'primary',
+                callback: () => {
+                    closePopup();
+                }
+           
+            }]));
+        }
+        else
+        {
+            allPopup.push(WA.ui.openPopup("tresorPopup1", "Le trésor est.. le nom de ce fameux chanteur !", [{
+                label: "Découvrir le trésor !",
+                className: 'primary',
+                callback: () => {
+                    WA.openTab("https://www.youtube.com/embed/dQw4w9WgXcQ?si=UvOTyPhvzyGRv-6r" ),
+                    closePopup();
+                }
+            }]));
+        }
+        })
+    
+
+    WA.room.area.onLeave('tresor').subscribe(closePopup)
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
     }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
+
+
 
 
 WA.room.onEnterLayer("mgZoneAdmin").subscribe(() => {
@@ -389,6 +554,7 @@ WA.room.onEnterLayer("mgZoneAdmin").subscribe(() => {
  */
 WA.room.onLeaveLayer("mgZoneAdmin").subscribe(() => {
     allPopup.forEach((element) => element.close());
+    allPopup.slice(0, allPopup.length)
 })
 WA.ui.onRemotePlayerClicked.subscribe((remotePlayer) => {
     remotePlayer.addAction('Ask to tell a joke', () => {
@@ -401,10 +567,10 @@ WA.ui.onRemotePlayerClicked.subscribe((remotePlayer) => {
  * ferme le popup courant
  */
 function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
+    allPopup.forEach((element =>{
+        element.close()
+    }))
+    allPopup.splice(0, allPopup.length)
 }
 /**
  * 
